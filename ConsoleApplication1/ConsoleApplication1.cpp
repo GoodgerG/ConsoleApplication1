136eb72af73141516c252eaba50500c34f80fa80
Goodger <olberg@sfedu.ru>
2022-05-25 23:19:22 +0300

    <комментарий>

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,24 @@
+﻿
+#include <iostream>
+#include <string>
+#include <sstream>
+
+using namespace std;
+
+int main()
+{
+    stringstream strm1;
+    strm1 << "London is the capital of Great Britain\n";
+    
+    string ss;
+
+    while (strm1 >> ss) {
+        //strm1 >> ss;
+        cout << ss << endl;
+    }
+
+    
+
+    return 0;
+}
+
